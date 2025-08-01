f731f8c69e11b2a9a7d35e07d121a17815e9ded4
Taboy9916 <113427835+Taboy9916@users.noreply.github.com>
2025-08-02 01:19:31 +0700

    Update LanguageContext.jsx

--- src/contexts/LanguageContext.jsx
+++ src/contexts/LanguageContext.jsx
@@ -1,1 +1,20 @@
+import React, { createContext, useState, useContext } from 'react';
 
+const LanguageContext = createContext();
+
+export const LanguageProvider = ({ children }) => {
+  const [language, setLanguage] = useState('lo'); // Default language is Lao
+
+  const changeLanguage = (newLanguage) => {
+    setLanguage(newLanguage);
+    // Here you might also want to save the language preference to localStorage
+  };
+
+  return (
+    <LanguageContext.Provider value={{ language, changeLanguage }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
+
+export const useLanguage = () => useContext(LanguageContext);
